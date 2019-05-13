972124329992c253499d81acff90543b67dea202
cr33dx <shashwatmahar12@gmail.com>
2019-05-13 17:50:11 +0530

    remoce

--- euclid.py
+++
@@ -1,9 +0,0 @@
-def gcd(m,n):
-	if(m<n):
-		(m,n)=(n,m)
-	if(m%n==0):
-		return n
-	else:
-		return gcd(n,m%n)
-
-print(gcd(90,50))
--- rotatelist.py
+++
@@ -1,4 +0,0 @@
-def rotatelist(list,length):
-	for i in range(0,length):
-		list = list[1:] + list[0:1]
-	return list
